07ed3080861ea3b70efdf1e5596165fa656fe66e
eduardo merchan <eduardomerchansalamea@gmail.com>
2026-04-16 10:32:19 -0400

    sjsjs

--- log_generator.py
+++ log_generator.py
@@ -1,13 +1,18 @@
 import time
 import logging
-import faker
-def generate_normal(): #this will generate normal logs for use
+from faker import Faker
 
 
 
-def generate_bad():#this will generate malicious logs
+
+
+#def generate_normal(): #this will generate normal logs for use
 
 
 
-def generate_daily()#this will chosse a mix of malicious or normal logs for the use to the daily file
+#def generate_bad():#this will generate malicious logs
+
+
+
+#def generate_daily()#this will chosse a mix of malicious or normal logs for the use to the daily file
 
