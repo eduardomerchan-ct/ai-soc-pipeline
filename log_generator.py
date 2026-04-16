e28a903629cf3b3e4d963d25e57677539162c972
eduardo merchan <eduardomerchansalamea@gmail.com>
2026-04-16 10:23:03 -0400

    Creating all fucntiopns

--- log_generator.py
+++ log_generator.py
@@ -1,1 +1,13 @@
 import time
+import logging
+
+def generate_normal(): #this will generate normal logs for use
+
+
+
+def generate_bad():#this will generate malicious logs
+
+
+
+def generate_daily()#this will chosse a mix of malicious or normal logs for the use to the daily file
+
